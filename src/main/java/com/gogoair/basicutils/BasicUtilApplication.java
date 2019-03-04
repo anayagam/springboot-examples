a697e23a1c1b34575933d4ae904de7347347fcbf
anayagam <anayagam@gogoair.com>
2019-03-04 11:16:51 -0600

    first commit

---
+++ src/main/java/com/gogoair/basicutils/BasicUtilApplication.java
@@ -0,0 +1,19 @@
+package com.gogoair.basicutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BasicUtilApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder applicationBuilder){
+        return applicationBuilder.sources(BasicUtilApplication.class);
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(BasicUtilApplication.class, args);
+    }
+}
---
+++ src/main/java/com/gogoair/basicutils/presentation/Controller.java
@@ -0,0 +1,12 @@
+package com.gogoair.basicutils.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping({"/","/ping"})
+    public String ping(){
+        return "pong";
+    }
+}
