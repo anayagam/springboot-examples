d2a68aefd485c42d849b02fe0979b3f4e20dc0a8
Anoop Nayagam <anayagam@gogoair.com>
2019-03-04 15:49:24 -0600

    Update Controller.java

--- src/main/java/com/coding/basicutils/presentation/Controller.java
+++ src/main/java/com/coding/basicutils/presentation/Controller.java
@@ -1,4 +1,4 @@
-package com.gogoair.basicutils.presentation;
+package com.coding.basicutils.presentation;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
