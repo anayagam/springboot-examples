0a29023d1d86925de5677688e1580ccf45256c16
anayagam <anayagam@gogoair.com>
2019-03-07 12:42:17 -0600

    adding logging - sl4j

--- src/main/java/com/coding/basicutils/presentation/Controller.java
+++ src/main/java/com/coding/basicutils/presentation/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping({"/","/ping"})
     public String ping(){
+        LOG.info("Successful get call made to Basic App Controller : {}", "App is up and running");
         return "pong";
     }
 }
